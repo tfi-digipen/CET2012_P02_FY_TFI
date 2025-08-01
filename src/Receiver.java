fd6e59114395ea82b4b3f9e9da701eaa3a5d20f2
TFI <t.indralie@digipen.edu>
2025-08-01 14:21:54 +0800

    Update

--- src/Receiver.java
+++ src/Receiver.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class Receiver {
+    public AddCommand addCommand;
+    public DeleteCommand deleteCommand;
+    public UpdateCommand updateCommand;
+    public ListCommand listCommand;
+    public UndoCommand undoCommand;
+
+    public Stack<Command> commandStack;
+    public ArrayList<String> dataStore;
+
+    public Receiver() {
+
+    }
 }
